e756169f5c3368a4d8301bfd5ad5896ad4313135
Rezetchik <lokipirat@mail.ru>
2024-06-23 23:03:17 +0300

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import style from './scss/App.module.scss';
+import { Suspense, lazy } from 'react';
 
-import Case from './components/CaseBox';
-import Tabs from './components/Tabs';
-import Rules from './components/Rules';
-import Keys from './components/Keys';
-import Exchange from './components/Exchange';
+// import Case from './components/CaseBox';
+// import Tabs from './components/Tabs';
+// import Rules from './components/Rules';
+// import Keys from './components/Keys';
+// import Exchange from './components/Exchange';
 
 import { Routes, Route, Navigate } from 'react-router-dom';
+
+const Case = lazy(() => import('./components/CaseBox'));
+const Tabs = lazy(() => import('./components/Tabs'));
+const Rules = lazy(() => import('./components/Rules'));
+const Keys = lazy(() => import('./components/Keys'));
+const Exchange = lazy(() => import('./components/Exchange'));
 
 const tg = window.Telegram.WebApp;
 
@@ -32,10 +39,38 @@ const App = () => {
         <Tabs />
       </div>
       <Routes>
-        <Route path="/rule" element={<Rules />} />
-        <Route path="/" element={<Case />} />
-        <Route path="/exchange" element={<Exchange />} />
-        <Route path="/keys" element={<Keys />} />
+        <Route
+          path="/rule"
+          element={
+            <Suspense fallback={<></>}>
+              <Rules />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<></>}>
+              <Case />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/exchange"
+          element={
+            <Suspense fallback={<></>}>
+              <Exchange />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/keys"
+          element={
+            <Suspense fallback={<></>}>
+              <Keys />
+            </Suspense>
+          }
+        />
         {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
       </Routes>
     </div>
